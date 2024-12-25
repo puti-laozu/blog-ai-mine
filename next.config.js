3af55852ba29be5c4d29ebfc5cbb18edfa25efd8
iallu <54088631+iallu@users.noreply.github.com>
2024-12-25 16:16:59 +0800

    20

--- next.config.js
+++ next.config.js
@@ -2,7 +2,17 @@
 const nextConfig = {
   images: {
     domains: ['raw.githubusercontent.com', 'github.com'],
-  }
+  },
+  experimental: {
+    appDir: true,
+  },
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': './app',
+    };
+    return config;
+  },
 }
 
 module.exports = nextConfig 
