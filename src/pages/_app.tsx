092a87086fcc434efbeb24fe3c50de32a5e996b7
iallu <54088631+iallu@users.noreply.github.com>
2024-12-25 13:25:59 +0800

    53620

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,6 @@
-import { AppProps } from 'next/app';
-import Layout from '../components/layout/Layout';
+import type { AppProps } from 'next/app';
 import '../styles/globals.css';
 
-export default function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
-  );
+export default function App({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />;
 } 
